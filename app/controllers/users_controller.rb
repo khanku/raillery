691ee2e00520e7def3e4f3bb7b749be0780280ad
Francois CHAVANT <code@chavant.info>
2009-08-21 20:07:19 +0200

    navigation implemented in users view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,11 +29,24 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     @username = user.login
 
-    @pictures = user.pictures
-    @pictures_in_a_row = get_setting('pictures_in_a_row')
+    @page = params[:page].to_i ||= 1
+    if (@page < 1)
+      @page = 1
+    end
+
+    @pictures_per_page = get_setting('pictures_per_page').to_i
+    offset = (@page - 1) * @pictures_per_page
+
+    @pictures = Picture.find_all_by_user_id(user.id,
+					    :order  => "created_at DESC",
+					    :limit  => @pictures_per_page,
+					    :offset => offset
+					   )
+    @pictures_count = @pictures.count
+    @pictures_in_a_row = get_setting('pictures_in_a_row').to_i
   end
 
   def index
-   @users_count = User.count 
+   @users_count = User.count
   end
 end
