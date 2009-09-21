2a2e74430d7f4017c4fd0d30b0940c7bdb6ee37c
Francois CHAVANT <code@chavant.info>
2009-09-21 17:08:22 +0200

    add album name as alt attribute in albums listing

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,9 +6,10 @@ module UsersHelper
 
     if album.pictures.count > 0
       str = image_tag pictures_thumb_dir +
-              "#{album.user.login}/#{album.pictures.first.filename}"
+              "#{album.user.login}/#{album.pictures.first.filename}",
+              :alt => 'Album: ' + h(album.name)
     else
-      str = image_tag('empty.png')
+      str = image_tag('empty.png', :alt => 'This album is still empty.')
     end
 
     return str
